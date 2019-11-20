0936b8faa191462868d932bfa291d3385fa981a7
subsevy <sub0713@gmail.com>
2019-11-21 01:28:17 +0900

    CSS in React 3

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,12 +5,14 @@ import {
   Redirect,
   Switch
 } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" component={TV} />
