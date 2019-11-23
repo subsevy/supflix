97993fad66012a5f8e7692fdd91e532d70ca0ef2
subsevy <sub0713@gmail.com>
2019-11-24 00:44:36 +0900

    Detail Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -19,7 +19,7 @@ export default () => (
       <Route path="/tv" component={TV} />
       <Route path="/search" component={Search} />
       <Route path="/movie/:id" component={Detail} />
-      <Route path="/tv/:id" component={Detail} />
+      <Route path="/show/:id" component={Detail} />
       <Redirect from="*" to="/" />
     </Switch>
   </Router>
