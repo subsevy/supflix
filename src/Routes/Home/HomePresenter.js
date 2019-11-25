0fbc53c03929edc81f7503c531b67042005f4854
subsevy <sub0713@gmail.com>
2019-11-25 21:35:48 +0900

    Message Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,9 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
 import Section from "Components/Section";
-import Loader from "../../Components/Loader";
+import Loader from "Components/Loader";
+import Message from "Components/Message";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -35,8 +35,10 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
           ))}
         </Section>
       )}
+      {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
+
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
   upcoming: PropTypes.array,
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -15,19 +16,26 @@ const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map(show => show.name)}
+          {topRated.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Shows">
-          {popular.map(show => show.name)}
+          {popular.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today Shows">
-          {airingToday.map(show => show.name)}
+          {airingToday.map(show => (
+            <span key={show.id}>{show.name}</span>
+          ))}
         </Section>
       )}
+      {error && <Message color="#e74c3c" text={error} />}
     </Container>
   );
 
