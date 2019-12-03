4af6eb68c5ca1736e5b867914f3c8ef550096f98
subsevy <sub0713@gmail.com>
2019-12-04 04:13:48 +0900

    Updates

---
+++ precache-manifest.c26e6af8d44836dfb93b161de7c95518.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d68909e783232a7a5967205562f474c",
+    "url": "/supflix/index.html"
+  },
+  {
+    "revision": "ecb2fa4d3dda23655fcc",
+    "url": "/supflix/static/js/2.5c4c8606.chunk.js"
+  },
+  {
+    "revision": "f0c8a204591aed751a42",
+    "url": "/supflix/static/js/main.0a2b2782.chunk.js"
+  },
+  {
+    "revision": "96054412c02972d1a8c8",
+    "url": "/supflix/static/js/runtime-main.3c5ec581.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/supflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
